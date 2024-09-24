cc75c8cceb51f868cea8212a4d45196d8de04c62
abblancodos <abblancodos@gamil.com>
2024-09-24 15:55:10 -0600

    design_filter.m con todos los filtros terminados tentativamente

--- design_filter.m
+++ design_filter.m
@@ -33,7 +33,7 @@ Atten = 50;
 # Lowpass
 [B,A] = ellip(Order,Ripple, Atten, Flp/(Fs/2));
 SOS = tf2sos(B,A);
-save("./mat-files/ellip_lp.mat", "SOS");        # Guarda el archivo en la carpeta de mat-files con el respectivo nombre
+save("./mat-files/ellip_lp.mat", "SOS");      # Guarda el archivo en la carpeta de mat-files con el respectivo nombre
 
 # Highpass
 [B,A] = ellip(Order,Ripple, Atten, Fhp/(Fs/2), 'high');
@@ -49,3 +49,69 @@ save("./mat-files/ellip_bp.mat", "SOS");
 [B,A] = ellip(Order,Ripple,Atten,[Flb/(Fs/2) Fub/(Fs/2)],'stop');
 SOS = tf2sos(B,A);
 save("./mat-files/ellip_bs.mat", "SOS");
+
+# Butterworth Filters
+# Lowpass
+[B,A] = butter(Order,Flp/(Fs/2),'low');
+SOS = tf2sos(B,A)
+save("./mat-files/butter_lp.mat", "SOS");
+
+
+# Highpass
+[B,A] = butter(Order,Flp/(Fs/2),'high');
+SOS = tf2sos(B,A)
+save("./mat-files/butter_hp.mat", "SOS");
+
+# Bandpass
+[B,A] = butter(Order,[Flb/(Fs/2) Fub/(Fs/2)],'bandpass');
+SOS = tf2sos(B,A)
+save("./mat-files/butter_bp.mat", "SOS");
+
+# Bandstop
+[B,A] = butter(Order,[Flb/(Fs/2) Fub/(Fs/2)],'stop');
+SOS = tf2sos(B,A)
+save("./mat-files/butter_bs.mat", "SOS");
+
+# Chebyshev Type I
+#LowPass
+[B,A] = cheby1(Order, Ripple, Flp/(Fs/2), 'low');
+SOS = tf2sos(B,A)
+save("./mat-files/cheby1_lp.mat", "SOS");
+
+#HighPass
+[B,A] = cheby1(Order, Ripple, Flp/(Fs/2), 'low');
+SOS = tf2sos(B,A)
+save("./mat-files/cheby1_hp.mat", "SOS");
+
+#BandPass
+[B,A] = cheby1(Order, Ripple, [Flb/(Fs/2) Fub/(Fs/2)], 'low');
+SOS = tf2sos(B,A)
+save("./mat-files/cheby1_bp.mat", "SOS");
+
+#StopBand
+[B,A] = cheby1(Order, Ripple, [Flb/(Fs/2) Fub/(Fs/2)], 'low');
+SOS = tf2sos(B,A)
+save("./mat-files/cheby1_bs.mat", "SOS");
+
+# Chebyshev Type II
+#LowPass
+[B,A] = cheby2(Order, Ripple, Flp/(Fs/2), 'low');
+SOS = tf2sos(B,A)
+save("./mat-files/cheby2_lp.mat", "SOS");
+
+#HighPass
+[B,A] = cheby2(Order, Ripple, Flp/(Fs/2), 'low');
+SOS = tf2sos(B,A)
+save("./mat-files/cheby2_hp.mat", "SOS");
+
+#BandPass
+[B,A] = cheby2(Order, Ripple, [Flb/(Fs/2) Fub/(Fs/2)], 'low');
+SOS = tf2sos(B,A)
+save("./mat-files/cheby2_bp.mat", "SOS");
+
+#StopBand
+[B,A] = cheby2(Order, Ripple, [Flb/(Fs/2) Fub/(Fs/2)], 'low');
+SOS = tf2sos(B,A)
+save("./mat-files/cheby2_bs.mat", "SOS");
+
+
