a19a568bf653b99999ab0c5444ba0bf05f261981
OscarM2023 <oscarmariogc@yahoo.com>
2024-10-01 14:58:40 -0600

    Visualizador de respuesta en frecuencia terminado

--- viewfreqresp.m
+++ viewfreqresp.m
@@ -31,15 +31,47 @@ H = num./den;
 # Magnitud en decibeles
 H_dB = 20*log10(abs(H));
 
+# Fase (normalizada en 2*pi y -2*pi)
+
+H_ph = mod(rad2deg(arg(H)),-360);
+
+# Eje x del plot de frecuencia:
+
+x_axis=w.*(Fs/(2*pi));
+
+figure(1)
+
 # Primer plot de la magnitud en dB
 subplot(2, 1, 1);
-semilogx(w.*(Fs/(2*pi)),H_dB,"Linewidth",3);
+semilogx(x_axis,H_dB,"Linewidth",3);
 
-# Configuración de los ejes y titulos
+#Configuración de los ejes y títulos del primer plot
+
 xlim([1 w(end)*(Fs/(2*pi))]);
-ylim([-100 5]);
+ylim([-100,5]);
+title("Respuesta en magnitud del filtro: ")
 xlabel('Frecuencia (Hz)');
 ylabel('Magnitud (dB)');
+
+# Segundo plot de la fase
+subplot(2, 1, 2);
+semilogx(x_axis,H_ph,"Linewidth",3);
+
+#Configuración de los ejes y tíulos del segundo plot
+
+xlim([1,x_axis(columns(w.*(Fs/(2*pi))))]);
+ylim([-360,0]);
+title("Respuesta en fase del filtro: ")
+xlabel('Frecuencia (Hz)');
+ylabel('Fase (rad)');
+
+grid on
+
+figure(2);
+
+zplane(B,A);
+title("Diagrama de polos y ceros del filtro")
+
 grid on;
 
 
